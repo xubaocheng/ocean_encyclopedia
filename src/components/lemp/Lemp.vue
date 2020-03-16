492309de9165e21e0313515a7c2ff67d54266bb1
Bates <xth5516739@163.com>
2020-03-16 10:08:29 +0800

    海洋百科跑马灯时间30S

--- src/components/lemp/Lemp.vue
+++ src/components/lemp/Lemp.vue
@@ -40,7 +40,7 @@ export default {
     methods: {
        //移动
         move() {
-            this.timer = setInterval(this.starmove, 30)
+            this.timer = setInterval(this.starmove, 3000)
         },
         //开始移动
         starmove() {
